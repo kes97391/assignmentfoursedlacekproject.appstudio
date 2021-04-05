31c8095e3e8266ea4952f44c54d35e7d8481d0f8
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-05 09:41:31 -0500

    Halfway
    commit

---
+++ forms/states/states.js
@@ -0,0 +1,50 @@
+
+states.onshow=function(){
+drpStates.clear()
+query = "SELECT * FROM customer"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kes97391&pass=" + pw + "&database=kes97391&query=" + query)
+  
+  if (req.status == 200) {
+    console.log(`req.responseText is a JSON string that looks like this: ${req.responseText}`)
+  results = JSON.parse(req.responseText)
+  console.log(`The parsed JSON string is converted to a JS object (an array or arrays): ${results} where results[0] is ${results[0]}, the first array in the JS results object.`)
+  
+  if (results.length == 0)
+    lblErrorMessage.value = "There are no customers in database"
+  else {
+  let message = ""
+  for (i = 0; i < results.length; i++)
+    drpStates.addItem(message + results[i][4] + "\n")
+    }
+  } else 
+      lblErrorMessage.value = "Error Code": + req.status
+}
+
+drpStates.onclick=function(){
+  if (typeof(s) == "object")
+    return 
+  else 
+    drp.States.value = s
+    let state = drp.States.value 
+    
+query = "SELECT * FROM customer WHERE state = " + "" + state + ""
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kes97391&pass=" + pw + "&database=kes97391&query=" + query)
+  
+  if (req.status == 200) {
+    console.log(`req.responseText is a JSON string that looks like this: ${req.responseText}`)
+  results = JSON.parse(req.responseText)
+    console.log(`The parsed JSON string is converted to a JS object (an array of arrays): ${results} where results[0] is ${results[0]}, the first array in the JS results object.`)
+    
+  if (results.length == 0)
+    lblErrorMessage.value = "There are no customers in the database"
+  else {
+control:
+  let message = ""
+  for (i = 0; i < results.length; i++)
+    message = message + results[i][1] + "\n"
+    lstgCustomers.addItem(message)
+    }
+  } else 
+    lblErrorMessage = "Error Code:" + req.status
+}
+}
