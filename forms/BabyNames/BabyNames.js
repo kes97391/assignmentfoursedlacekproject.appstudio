dfa6a7257ca82f53218676a0cfe70f6237dc417b
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-05 19:48:48 -0500

    fix up

--- forms/BabyNames/BabyNames.js
+++ forms/BabyNames/BabyNames.js
@@ -1,13 +1,16 @@
-let babyNames = ["Olivia", "Emma", "Ava", "Sophia", "Isabella", "Charlotte", "Amelia", "Mia", "Harper", "Evelyn", "Abigail", "Emily", "Ella", "Elizabeth", "Camila", "Luna", "Sofia", "Avery", "Mila", "Aria"]
-let favBabyNames = []
+//Construct an array with common baby names, as well as an array designated for output. When the form is shown, it will display these baby names in the select. 
+//When a specific name is chosen, the form will change to the favBabyNames form.
 
-BabyNames.onshow=function(){
-  for (i = 0; i <babyNames.length; i++)
-    selBabyNames.addItem(babyNames[i])
+let BabyNames = ["Olivia", "Emma", "Ava", "Sophia", "Isabella", "Charlotte", "Amelia", "Mia", "Harper", "Evelyn", "Abigail", "Emily", "Ella", "Elizabeth", "Camila", "Luna", "Sofia", "Avery", "Mila", "Aria"]
+let FavBabyNames = []
+
+babyNames.onshow=function(){
+  for (i = 0; i < BabyNames.length; i++)
+    selBabyNames.addItem(BabyNames[i])
 }
 
 
 btnBabyNames.onclick=function(){
-  favBabyNames.push(selBabyNames.text)
+  FavBabyNames.push(selBabyNames.text)
   ChangeForm(favBabyNames)
 }
--- forms/Global/Global.js
+++ forms/Global/Global.js
@@ -2,5 +2,5 @@ let req = ""
 let query = ""
 let results = ""
 let pw = "Odie6343!"
-
+let netID = "kes97391"
 let allCustomerData = []
